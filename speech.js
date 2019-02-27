ee78645c08d249b2afb42d84dcfa39846903d7dc
Glitch (jazzy-cut) <none>
2019-02-27 04:23:52 +0000

    🐬👝 Checkpoint ./eliza.js:3282082/21940 ./speech.js:3282082/3169 ./index.html:3282082/94

---
+++ speech.js
@@ -0,0 +1,99 @@
+var yourTalk = document.getElementById('yourTalk');
+var startButton = document.getElementById('startButton');
+
+var say = (function() {
+    var voice = speechSynthesis.getVoices()[0];
+    return function(s,v=voice) {
+        var u = new SpeechSynthesisUtterance(s);
+        u.voice = v;
+        return new Promise(function(res,rej) {
+            speechSynthesis.speak(u);
+            u.addEventListener('end',res);
+        });
+    };
+})();
+
+var sleep = function(t) {
+    return new Promise(r=>setTimeout(r,t));
+}
+
+var translate = function(q,s,t) {
+    return fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${s}&tl=${t}&dt=t&q=`+encodeURI(q)).then(r=>r.json()).then(r=>r[0][0][0]);
+}
+
+/*var recognition = new webkitSpeechRecognition();
+recognition.interimResults=false;
+recognition.addEventListener('result',e=>console.log(e.results[0][0].transcript));/*e=>console.log(Array.from(e.results).map(u=>u.)));*/
+/*recognition.addEventListener('end',console.log);
+recognition.start()*/
+
+var listen = function(l) {
+    var recog = new window.webkitSpeechRecognition();
+    recog.interimResults = true;
+    var text = '';
+    recog.addEventListener('result',e=>(text=e.results[0][0].transcript));
+    if (l) {
+        recog.addEventListener('result',l);
+    }
+    return new Promise(function(res,rej) {
+        recog.addEventListener('end',()=>res(text));
+        recog.start();
+    });
+};
+
+var cycle = async function() {
+    yourTalk.style.color='grey';
+    var tex = await listen(e=>(yourTalk.innerText=e.results[0][0].transcript));
+    console.log(tex);
+    yourTalk.style.color='black';
+    var temp;
+    if (tex == 'stop') {
+        await say('alright');
+        startButton.hidden=false;
+        yourTalk.innerText='';
+        return true;
+    }
+    if (tex == 'open the pod bay doors Hal') {
+        await say('I\'m sorry, Dave. I\'m afraid I can\'t do that.');
+        return;
+    }
+    if (tex == 'who are you') {
+        await say("Me?");
+        await say("I know who I am. I'm a dude playing a dude disguised as another dude.");
+        return;
+    }
+    if (tex == 'update') {
+        await say('alright');
+        location.href=location.href;
+    }
+    if (temp=tex.split('repeat after me')[1]) {
+        await(say(temp));
+        return;
+    }
+    if (tex == 'who the hell are you') {
+        await say("I'm Batman");
+        return;
+    }
+    if (temp=tex.split('say in French')[1]) {
+        console.log(temp);
+        temp = await translate(temp,'en','fr');
+        console.log(temp);
+        await say(temp,speechSynthesis.getVoices().filter(i=>i.lang=='fr-FR')[0]);
+        return;
+    }
+    await sleep(1000+Math.random()*1000);
+    await say(listeneliza(tex));
+}
+
+var main = async function() {
+    console.log('started');
+    yourTalk.innerText='Please wait';
+    await say('Hello, I am HAL over 9000.');
+    yourTalk.innerText='';
+    while (true) {
+        if (await cycle()) {
+            break;
+        }
+        yourTalk.innerText='';
+    }
+}
